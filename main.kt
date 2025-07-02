421a0ea9faafbac96cabcbb69101fbd8a5483ff0
kintrixters <237919624+kintrixters@users.noreply.github.com>
2025-07-02 20:27:00 +0700

    fix: Perbaiki salah ketik kecil di dokumentasi di main.kt

---
+++ main.kt
@@ -0,0 +1,12 @@
+// main.kt - Skrip awal Kotlin yang komprehensif
+data class Project(val name: String, val version: String)
+fun main() {
+    val p = Project("Proyek Repositori Otomatis GitHub", "1.0.0")
+    println("Proyek: ${p.name}, Versi: ${p.version}")
+    println("\nFitur: Data Class, Fungsi main, Perulangan")
+    for (i in 1..5) {
+        println("  - Iterasi $i")
+    }
+}
+
+# Diubah pada 2025-07-02T00:00:00
