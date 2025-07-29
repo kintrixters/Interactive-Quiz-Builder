fc6cd8e1934fd1374478744e7b653110e93fb36a
kintrixters <237919624+kintrixters@users.noreply.github.com>
2025-07-29 22:13:00 +0700

    fix: Perbaiki salah ketik kecil di dokumentasi di main.rs

---
+++ main.rs
@@ -0,0 +1,18 @@
+// main.rs - Skrip awal Rust yang komprehensif
+struct Project {
+    name: String,
+    version: String,
+}
+fn main() {
+    let p = Project {
+        name: String::from("Proyek Repositori Otomatis GitHub"),
+        version: String::from("1.0.0"),
+    };
+    println!("Proyek: {}, Versi: {}", p.name, p.version);
+    println!("\nFitur: Struct, Kepemilikan, Perulangan");
+    for i in 0..5 {
+        println!("  - Iterasi {}", i + 1);
+    }
+}
+
+# Diubah pada 2025-07-29T00:00:00
